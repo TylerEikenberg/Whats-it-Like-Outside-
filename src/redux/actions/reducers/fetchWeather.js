76801d72bb362a882e668c90bebcc8e111751215
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-10 22:24:51 -0500

    setting up reducer for fetchWeather

---
+++ src/redux/actions/reducers/fetchWeather.js
@@ -0,0 +1,18 @@
+const initState = {
+  data: [],
+  loading: false,
+  error: null
+};
+
+const fetchWeatherReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'FETCH_WEATHER_BEGIN':
+      return { ...state, loading: true };
+    case 'FETCH_WEATHER_SUCCESS':
+      return { ...state, loading: false, data: action.payload };
+    case 'FETCH_WEATHER_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
