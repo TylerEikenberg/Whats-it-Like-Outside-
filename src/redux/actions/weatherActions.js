5af9daec65b1ccac695b0ca94932bf536ae0a72c
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-10 22:41:44 -0500

    added async fetch action

--- src/redux/actions/weatherActions.js
+++ src/redux/actions/weatherActions.js
@@ -14,8 +14,19 @@ export const fetchWeatherSuccess = data => {
   return { type: actions.FETCH_WEATHER_SUCCESS, payload: data };
 };
 
-export const fetchWeatherFailer = error => {
+export const fetchWeatherFailure = error => {
   return { type: actions.FETCH_WEATHER_FAILURE, payload: error };
 };
 
-export const fetchWeatherDataAsync = location => {};
+export const fetchWeatherDataAsync = location => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get(
+        `https://weather-api-tse.herokuapp.com/weather?address=${location}`
+      );
+      dispatch(fetchWeatherSuccess(data));
+    } catch (error) {
+      dispatch(fetchWeatherFailure(error.response));
+    }
+  };
+};
