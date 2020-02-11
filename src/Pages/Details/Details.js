7c0d1aa1f54b80428c3218df4291f9337b2f758d
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-11 18:56:43 -0500

    adding details to page

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -5,9 +5,35 @@ import './Details.css';
 
 // TODO:
 // fetch data on load of page incase user comes to this page without searching
+
+/**
+ * Details to include:
+ * 1.{data.location}
+          {data.data.currently.summary}
+          {data.data.currently.precipProbability}
+          {data.data.currently.temperature}
+          {data.data.currently.windSpeed}
+          {data.data.daily.data[0].summary}
+          {data.data.daily.data[0].temperatureHigh}
+          {data.data.daily.data[0].temperatureLow}
+          {data.data.daily.data[0].humidity}
+ */
 function Details({ match }) {
   const { data, loading, error } = useSelector(state => state.weatherReducer);
-  return <div>Details Page for {data.location}</div>;
+  return (
+    <div className='Details-page-wrapper'>
+      Details Page for location
+      <div className='Details-weather-container'>
+        <section className='Details-temp-box'>
+          <h1 className='Details-current-temp'>56.3</h1>
+          <div className='Details-high-low'>
+            <h5 className='Details-small-temp'>High: 58°f</h5>
+            <h5 className='Details-small-temp'>Low: 39°f</h5>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Details;
